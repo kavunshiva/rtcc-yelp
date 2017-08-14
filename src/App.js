2479e3f259fbfc20f65916e50de5297737b5ba3e
kavunshiva <kavunshiva@gmail.com>
2017-08-14 10:58:54 -0400

    add seantic ui

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { AuthAdapter } from './adapters'
-import NavBar from './components/NavBar'
-import LoginForm from './components/LoginForm'
 import RestaurantsContainer from './containers/RestaurantsContainer'
 
 class App extends Component {
@@ -18,21 +15,9 @@ class App extends Component {
     this.login = this.login.bind(this)
   }
 
-  login(params){
-    AuthAdapter.login(params)
-      .then(res => this.setState({
-        auth: {
-          isLoggedIn: true,
-          token: res.token
-        }
-      }))
-  }
-
   render() {
     return (
       <div className="App">
-        <NavBar />
-        <LoginForm onSubmit={this.login} />
         <RestaurantsContainer />
       </div>
     );
